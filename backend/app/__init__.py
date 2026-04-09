cdcc88bf2186ca4d74ca39f92c5070580d9bc100
Daniel Gonçalves <83424429+dev-danielg@users.noreply.github.com>
2026-04-09 10:40:58 -0300

    feat(backend): update create_app to load environment variables

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from .extensions import db, migrate, jwt
 from app.models.aluno import Aluno
-
+from dotenv import load_dotenv
+import os 
 
 def create_app() -> Flask:
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "mysql+pymysql://<YOUR_USERNAME>:<YOUR_PASSWORD>@localhost/sigac"
-    )
+    load_dotenv()
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
     db.init_app(app)
     migrate.init_app(app, db)
