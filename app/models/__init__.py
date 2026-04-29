5e4236755f753fdd07c11cb195577552e00999e2
Daniel Gonçalves <danielbrgoncalves@gmail.com>
2026-04-29 12:17:32 -0300

    refactor: moving backend to repository source

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .usuario import Usuario
+from .coordenador import Coordenador
+from .aluno import Aluno
+from .curso import Curso
+from .aluno_curso import AlunoCurso
+from .coordenador_curso import CoordenadorCurso
+from .submissao import Submissao
--- backend/app/extensions/__init__.py
+++
@@ -1,3 +0,0 @@
-from .db import Base, db
-from .migrate import migrate
-from .jwt import jwt
