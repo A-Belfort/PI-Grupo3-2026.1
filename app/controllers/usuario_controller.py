148736accdd5a211d173d6ab886838becd7dce9f
Daniel Gonçalves <danielbrgoncalves@gmail.com>
2026-05-03 07:59:20 -0300

    fix: add role validation for user registration in cadastrar_usuario_controller

--- app/controllers/usuario_controller.py
+++ app/controllers/usuario_controller.py
@@ -1,11 +1,35 @@
 from app.extensions import db
 from app.models import Usuario
 from werkzeug.security import generate_password_hash
+from flask_jwt_extended import get_jwt
+
 
 def cadastrar_usuario_controller(data):
+    role = get_jwt().get("role")
+
+    tipo_novo_usuario = data["tipo"]
+
+    if role == "coordenador" and tipo_novo_usuario != "aluno":
+        return {
+            "success": False,
+            "message": "Coordenador só pode cadastrar alunos."
+        }, 403
+        
+    if role == "super_admin" and tipo_novo_usuario not in ("aluno", "coordenador"):
+        return {
+            "success": False,
+            "message": "Tipo de usuário inválido."
+        }, 400
+        
+    if tipo_novo_usuario == "aluno" and not data.get("matricula"):
+        return {
+            "success": False,
+            "message": "Aluno precisa de matrícula."
+        }, 400    
+    
     senha_hash = generate_password_hash(data["senha"])
 
-    new_usuario = Usuario(
+    novo_usuario = Usuario(
         nome=data["nome"],
         email=data["email"],
         senha=senha_hash,
@@ -13,7 +37,7 @@ def cadastrar_usuario_controller(data):
         matricula=data.get("matricula")
     )
 
-    db.session.add(new_usuario)
+    db.session.add(novo_usuario)
     db.session.commit()
 
     return {
